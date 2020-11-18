d75d4f5e143b0641ffc868704c4107e60b275d4a
Airouane <erwan.cadobts@gmail.com>
2020-11-18 14:39:02 +0100

    feat: add description user and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DescriptionComponent} from './description/description.component';
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'description', component: DescriptionComponent},
+  {path: '', component: AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { DescriptionComponent } from './description/description.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersListComponent
+    UsersListComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
